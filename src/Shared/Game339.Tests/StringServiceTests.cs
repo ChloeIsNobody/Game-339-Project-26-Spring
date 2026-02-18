b54aa7bb7058d4fecbdde15ec7399f7da9880422
Chloe Weber <chloeisnobody@gmail.com>
2026-02-18 15:56:11 -0800

    Fixed Unit Tests

--- src/Shared/Game339.Tests/StringServiceTests.cs
+++ src/Shared/Game339.Tests/StringServiceTests.cs
@@ -33,29 +33,86 @@ public class StringServiceTests
         Assert.Throws<System.ArgumentNullException>(() => _svc.Reverse(null));
     }
     
-    [TestCase("hello", "hello")]
-    [TestCase("", "")]
-    [TestCase("a", "a")]
-    [TestCase("one two three", "three two one")]
-    [TestCase("one  two three", "three two  one")]
-    [TestCase(" one two three", "three two one ")]
-    [TestCase("one two three ", " three two one")]
-    [TestCase("one_two_three", "three_two_one")]
-    [TestCase("one two_three", "three_two one")]
-    [TestCase("one\ntwo\nthree", "three\ntwo\none")]
-    public void ReverseWords_ReturnsExpectedString(string input, string expected)
-    {
-        // Act
-        var result = _svc.Reverse(input);
-
-        // Assert
-        Assert.That(result, Is.EqualTo(expected));
-    }
+    [DatapointSource]
+    public static readonly char[] cases = [' ', '\n', '\r'];
 
     [Test]
-    public void ReverseWords_NullString_ThrowsArgumentNullException()
+    public void ReverseWords_Test_Null()
     {
         // Act & Assert
-        Assert.Throws<System.ArgumentNullException>(() => _svc.Reverse(null));
+        Assert.Throws<System.ArgumentNullException>(() => _svc.ReverseWords(null));
+    }
+    
+    [Test, TestCaseSource(nameof(cases))]
+    public void ReverseWords_Test_ZeroWords(char separator)
+    {
+        char s = separator;
+        
+        List<(string input, string output)> tests = [("", ""), ($"{s}", $"{s}"), ($"{s}{s}", $"{s}{s}"), ($"{s} ", $" {s}")];
+
+        foreach (var test in tests)
+        {
+            var result = _svc.ReverseWords(test.input);
+            
+            Assert.That(result, Is.EqualTo(test.output));
+        }
+    }
+    
+    [Test, TestCaseSource(nameof(cases))]
+    public void ReverseWords_Test_OneWords(char separator)
+    {
+        char s = separator;
+        
+        List<(string input, string output)> tests = [
+            ("one", "one"), ($"one'", $"one'"), ($"one's", $"one's"),
+            ($"{s}one", $"one{s}"), ($"one{s}", $"{s}one"), ($"{s}one{s}", $"{s}one{s}")
+        ];
+
+        foreach (var test in tests)
+        {
+            var result = _svc.ReverseWords(test.input);
+            
+            Assert.That(result, Is.EqualTo(test.output));
+        }
+    }
+    
+    [Test, TestCaseSource(nameof(cases))]
+    public void ReverseWords_Test_TwoWords(char separator)
+    {
+        char s = separator;
+        
+        List<(string input, string output)> tests = [
+            ($"one{s}two", $"two{s}one"),
+            ($"one{s}two{s}", $"{s}two{s}one"),
+            ($"{s}one{s}two", $"two{s}one{s}"),
+            ($"one_two", $"one_two")
+        ];
+
+        foreach (var test in tests)
+        {
+            var result = _svc.ReverseWords(test.input);
+            
+            Assert.That(result, Is.EqualTo(test.output));
+        }
+    }
+    
+    [Test, TestCaseSource(nameof(cases))]
+    public void ReverseWords_Test_ThreeWords(char separator)
+    {
+        char s = separator;
+        
+        List<(string input, string output)> tests = [
+            ($"one{s}two{s}three", $"three{s}two{s}one"),
+            ($"one{s}two{s}{s}three", $"three{s}{s}two{s}one"),
+            ($"one_two three", $"three one_two"),
+            ($"one{s}two{s} three", $"three {s}two{s}one")
+        ];
+
+        foreach (var test in tests)
+        {
+            var result = _svc.ReverseWords(test.input);
+            
+            Assert.That(result, Is.EqualTo(test.output));
+        }
     }
 }
