61db12f236b37ee46e1d176a45c0951e5307e6b7
Chloe Weber <chloeisnobody@gmail.com>
2026-02-17 19:09:14 -0800

    Added ReverseWords function

--- src/Shared/Game339.Shared/Runtime/Services/Implementation/StringService.cs
+++ src/Shared/Game339.Shared/Runtime/Services/Implementation/StringService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Game339.Shared.Diagnostics;
 
@@ -17,6 +18,29 @@ namespace Game339.Shared.Services.Implementation
             var output = new string(input.Reverse().ToArray());
             _log.Info($"{nameof(StringService)}.{nameof(Reverse)} - {nameof(input)}: {input} - {nameof(output)}: {output}");
             return output;
+        }
+
+        public string ReverseWords(string input)
+        {
+            string output = "";
+
+            (string text, bool isSeparator) currentWord = ("", false);
+
+            foreach (char letter in input)
+            {
+                if (isWordSeparator(letter) != currentWord.isSeparator)
+                {
+                    currentWord.isSeparator = isWordSeparator(letter);
+                    output = currentWord.text + output;
+                    currentWord.text = "";
+                }
+                
+                currentWord.text += letter;
+            }
+            
+            return output;
+
+            bool isWordSeparator(char c) => char.IsSeparator(c);
         }
     }
 }
--- src/Shared/Game339.Tests/StringServiceTests.cs
+++ src/Shared/Game339.Tests/StringServiceTests.cs
@@ -32,4 +32,30 @@ public class StringServiceTests
         // Act & Assert
         Assert.Throws<System.ArgumentNullException>(() => _svc.Reverse(null));
     }
+    
+    [TestCase("hello", "hello")]
+    [TestCase("", "")]
+    [TestCase("a", "a")]
+    [TestCase("one two three", "three two one")]
+    [TestCase("one  two three", "three two  one")]
+    [TestCase(" one two three", "three two one ")]
+    [TestCase("one two three ", " three two one")]
+    [TestCase("one_two_three", "three_two_one")]
+    [TestCase("one two_three", "three_two one")]
+    [TestCase("one\ntwo\nthree", "three\ntwo\none")]
+    public void ReverseWords_ReturnsExpectedString(string input, string expected)
+    {
+        // Act
+        var result = _svc.Reverse(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ReverseWords_NullString_ThrowsArgumentNullException()
+    {
+        // Act & Assert
+        Assert.Throws<System.ArgumentNullException>(() => _svc.Reverse(null));
+    }
 }
